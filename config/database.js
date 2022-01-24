81dbf528f48aa548e59fe5492050888c793748c1
Pengcheng (Philip) Hou <hphilip168@gmail.com>
2022-01-23 20:19:08 -0500

    submitting lab

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/movies',
-{ useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect('mongodb://127.0.0.1:27017/movies',
+{ useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true }
 );
 
 // shortcut to mongoose.connection object
--- query-practice.js
+++ query-practice.js
@@ -1,58 +1,76 @@
-// utility to initialize database
-require('./config/database');
-const Movie = require('./models/movie');
-const Performer = require('./models/performer');
-
+require('./config/database');// connect this script to the database
+ const Movie = require('./models/movie');
+ const Performer = require('./models/performer');
+ 
 /*-- For each exercise below, write the code as described --*/
 
 Promise.resolve().then(function() {
-  console.log('HERE')
-  // 1) Find all movie docs
-  return Movie.find({});  // remember to return the promise!
-}).then(function(result) {
-  console.log('1): ', result)
-  // 2) Find all performer docs
-
-}).then(function(result) {
-  console.log('2): ', result)
-  // Follow the same .then structure used above from this point forward
-  // Don't forget to console.log the exercise number also as shown above 
-  // 3) Find all movies with an MPAA Rating of 'PG'
+    console.log('HERE')
+    // 1) Find all movie docs
+    return Movie.find({});  // remember to return the promise!
+  }).then(function(result) {
+    console.log('1): ', result);
+    // 2) Find all performer docs
+    return Performer.find({});
+  }).then(function(result) {
+    console.log('2): ', result);
+    // Follow the same .then structure used above from this point forward
+    // Don't forget to console.log the exercise number also as shown above 
+    // 3) Find all movies with an MPAA Rating of 'PG'
+    return Movie.find ({mpaaRating:"PG"});
+  }).then(function(result){
+    console.log('3:',result);
+   // 4) Find all movies that are still showing
+    return Movie.find({ nowShowing: true })
+  }).then(function(result) {
+    console.log('4): ', result)
+    // 5) Find all movies with an MPAA Rating of 'PG' or 'PG-13'
+    return Movie.find({mpaaRating:{$in:['PG','PG-13']}})
+  }).then(function(result) {
+    console.log('5):',result);
+    // 6) Find the first movie found with a releaseYear of 2018
+    return Movie.findOne({ releaseYear: 2018 })
+  }).then(function(result) {
+    console.log('6): ', result)
+    // 7) Find all movies released after 1980
+    return Movie.find({ releaseYear: { $gt: 1980 }})
+  }).then(function(result) {
+    console.log('7): ', result)
+    // 8) Find all movies whose titles start with a 'C'
+    // Hint: StackOverflow will show how to use a regular expression
+    return Movie.find({ title: { $regex: /C/ }})
+  }).then(function(result) {
+    console.log('8): ', result)
+    // 9) Find the performer named 'Rami Malek'
+    return Performer.find({ name: 'Rami Malek'})
+  }).then(function(result) {
+    console.log('9): ', result)
+    // 10) Find all performers born before 1980
+    return Performer.find({ born: { $lte: '1980' }})
+  }).then(function(result) {
+    console.log('10): ', result)
+    // 11) Find all performers whose name starts with a 'J'
+    return Performer.find({ name: /^J/})
+  }).then(function(result) {
+    console.log('11): ', result)
+    // 12) Add a reference to performer 'Bill Murray' to
+    //     the movie Caddyshack's cast property and save.
+    //     console.log the updated movie.
+    return Promise.all([
+        Performer.findOne({ name: 'Bill Murray' }),
+        Movie.findOne({ title: 'Caddyshack' })
+    ]);
+  }).then(function(result) {
+      const pfmer = result[0]
+      const moVie = result[1]
+      moVie.cast.push(pfmer)
+      return moVie.save()
+  })
+  .then(function(result) {
+    console.log('12): ', result)
+    process.exit();
+  });
   
-})
-
-  // 4) Find all movies that are still showing
-
-
-  // 5) Find all movies with an MPAA Rating of 'PG' or 'PG-13'
-
-
-  // 6) Find the first movie found with a releaseYear of 2018
-
-
-  // 7) Find all movies released after 1980
-  
-
-  // 8) Find all movies whose titles start with a 'C'
-  // Hint: StackOverflow will show how to use a regular expression
-  
-
-  // 9) Find the performer named 'Rami Malek'
   
   
-  // 10) Find all performers born before 1980
   
-  
-  // 11) Find all performers whose name starts with a 'J'
-
-
-  // 12) Add a reference to performer 'Bill Murray' to
-  //     the movie Caddyshack's cast property and save.
-  //     console.log the updated movie.
-
-
-.then(function() {
-  process.exit();
-});
-
-
